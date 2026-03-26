e431ce1a565e0c5196895f1bb02dd6a808ef7c8a
RaySummer <ray__summer@hotmail.com>
2026-03-26 18:11:12 +0800

    修复bug

--- src/main/java/com/ray/atten/middle/module/utils/ExcelReader.java
+++ src/main/java/com/ray/atten/middle/module/utils/ExcelReader.java
@@ -11,9 +11,9 @@ public class ExcelReader {
 
     public static void main(String[] args) {
         // 1. Excel 文件路径
-        String fileName = "C:\\Users\\Ray\\Downloads\\oaEmployee.xlsx";
+        String fileName = "C:\\Users\\Ray\\Downloads\\attendanceLog.xlsx";
         // 2. 表名
-        String tableName = "oa_employee";
+        String tableName = "attendance_logs";
 
         // 3. 开始读取
         EasyExcel.read(fileName, new ReadListener<Map<Integer, String>>() {
@@ -21,14 +21,9 @@ public class ExcelReader {
             public void invoke(Map<Integer, String> data, AnalysisContext context) {
                 // data 是一个 Map，Key 是列号（从 0 开始），Value 是单元格内容
                 // 假设提取第 0 列 (姓名) 和 第 1 列 (年龄)
-                String name = data.get(0);
+                String verify_time = data.get(0);
                 String pin = data.get(1);
-                String dept = data.get(2);
-                String company = data.get(3);
-                String officeLocation = data.get(4);
-                String entryTime = data.get(5);
-                String post = data.get(6);
-                String uuid = UUID.randomUUID().toString();
+                String device_sn = data.get(2);
 
                 // 4. 拼装 SQL
 //                String sql = String.format(
@@ -36,9 +31,13 @@ public class ExcelReader {
 //                                " VALUES ('%s', '%s', '%s', '%s', '%s', 1, '%s', '%s', NOW(), NOW(), '%s');",
 //                        tableName, uuid, name, pin, dept, company, officeLocation, entryTime, post
 //                );
+//                String sql = String.format(
+//                        "UPDATE %s SET POST = '%s' WHERE PIN = '%s'; ",
+//                        tableName, post, pin
+//                );
                 String sql = String.format(
-                        "UPDATE %s SET POST = '%s' WHERE PIN = '%s'; ",
-                        tableName, post, pin
+                        "INSERT INTO %s(verify_time, user_pin, device_sn) VALUES ('%s', '%s', '%s');",
+                        tableName, verify_time, pin, device_sn
                 );
 
                 System.out.println(sql);
